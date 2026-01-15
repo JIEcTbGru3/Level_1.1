d205d501252d3a556650895b0573bc6ef8f57b1c
JIEcTbGru3 <nikitayankovskiy2317@gmail.com>
2026-01-15 12:34:46 +0300

    Level_1

--- src/level1/Task9.java
+++ src/level1/Task9.java
@@ -5,10 +5,10 @@ public class Task9 {
 
         for (float f = 1.0f; f < 2; f += 0.1) {
 
-            System.out.println("float: "+f);
+            System.out.println("float: " + f);
         }
         for (double d = 1.0; d < 2; d += 0.1) {
-            System.out.println("double: "+d);
-}
+            System.out.println("double: " + d);
+        }
     }
 }
