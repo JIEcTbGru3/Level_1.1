5299c5942bca08b2e75752443eee753ca51ef6e5
JIEcTbGru3 <nikitayankovskiy2317@gmail.com>
2026-01-16 09:48:54 +0300

    Level_2

---
+++ src/level_2/Task1.java
@@ -0,0 +1,19 @@
+package level_2;
+
+import java.util.Scanner;
+
+public class Task1 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        long hp = sc.nextLong();
+        long defense = sc.nextLong();
+        if (hp < defense) {
+            System.out.println("Орк побежден");
+        } else {
+            System.out.println("Беги, глупец!");
+        }
+
+    }
+
+
