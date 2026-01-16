9eb3edc3bf1ec6a0d9860842e218cf454c5df26b
JIEcTbGru3 <nikitayankovskiy2317@gmail.com>
2026-01-16 12:32:15 +0300

    Level_2

---
+++ src/lessons1/Test.java
@@ -0,0 +1,22 @@
+package lessons1;
+
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String srt = sc.nextLine();
+        int i = sc.nextInt();
+        double d = sc.nextDouble();
+        String st = sc.next();
+
+        System.out.println(srt);
+        System.out.println(i);
+        System.out.println(d);
+        System.out.println(st);
+
+
+    }
+
+}
---
+++ src/lessons1/Test1.java
@@ -0,0 +1,23 @@
+package lessons1;
+
+public class Test1 {
+   public static void main (String[] args) {
+
+       char c = 'a';
+       int a = 10;
+       int b = 5;
+       int d = ++a+b;
+       boolean b1 = a>b;
+       int f = a|b;
+
+       boolean x = true;
+       boolean y = true;
+       boolean z = false;
+
+       System.out.println(a+c);
+       System.out.println (d);
+       System.out.println (b1);
+       System.out.println (f);
+       System.out.println (false);
+   }
+}
